f4ad229b1881ad6216f0746b71c2906bbd2eaf13
Michael Murphy <michael.w.murphy@outlook.com>
2023-06-13 07:38:00 +0100

    Adds the service

---
+++ src/Blazored.Dialog/BlazoredDialogService.cs
@@ -0,0 +1,28 @@
+using Microsoft.JSInterop;
+
+namespace Blazored.Dialog;
+
+public class BlazoredDialogService : IBlazoredDialog, IAsyncDisposable
+{
+    private readonly Lazy<Task<IJSObjectReference>> _moduleTask;
+    
+    public BlazoredDialogService(IJSRuntime jsRuntime)
+    {
+        _moduleTask = new Lazy<Task<IJSObjectReference>>(() => jsRuntime.InvokeAsync<IJSObjectReference>(
+            "import", "./_content/Blazored.Dialog/blazoredDialog.js").AsTask());
+    }
+
+    public BlazoredDialog CreateNewDialog(string htmlDialogId)
+    {
+        return new BlazoredDialog(htmlDialogId, _moduleTask);
+    }
+    
+    public async ValueTask DisposeAsync()
+    {
+        if (_moduleTask.IsValueCreated)
+        {
+            var module = await _moduleTask.Value;
+            await module.DisposeAsync();
+        }
+    }
+}
