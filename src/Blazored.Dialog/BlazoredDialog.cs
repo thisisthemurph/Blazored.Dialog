a8b1fd06c19d2336585c1563782b3d51575784f1
Michael Murphy <michael.w.murphy@outlook.com>
2023-06-15 06:11:37 +0100

    Adds documentation to methods

--- src/Blazored.Dialog/BlazoredDialog.cs
+++ src/Blazored.Dialog/BlazoredDialog.cs
@@ -19,6 +19,9 @@ public class BlazoredDialog
         _moduleTask = moduleTask;
     }
 
+    /// <summary>
+    /// Displays the dialog modelessly, i.e. still allowing interaction with content outside of the dialog.
+    /// </summary>
     public async Task Show()
     {
         if (await DialogIdIsNull()) return;
@@ -26,6 +29,11 @@ public class BlazoredDialog
         await module.InvokeVoidAsync("blazoredDialog.show", DialogId);
     }
 
+    /// <summary>
+    /// Displays the dialog as a modal, over the top of any other dialogs that might be present. It displays in the
+    /// top layer, along with a ::backdrop pseudo-element. Interaction outside the dialog is blocked and the content
+    /// outside it is rendered inert.
+    /// </summary>
     public async Task ShowModal()
     {
         if (await DialogIdIsNull()) return;
@@ -33,6 +41,9 @@ public class BlazoredDialog
         await module.InvokeVoidAsync("blazoredDialog.showModal", DialogId);
     }
 
+    /// <summary>
+    /// Closes the dialog.
+    /// </summary>
     public async Task Close()
     {
         if (await DialogIdIsNull()) return;
@@ -40,6 +51,10 @@ public class BlazoredDialog
         await module.InvokeVoidAsync("blazoredDialog.close", DialogId);
     }
     
+    /// <summary>
+    /// Closes the HTMLDialogElement and updates the returnValue of the dialog.
+    /// </summary>
+    /// <param name="returnValue">Value to be stored in the dialog returnValue.</param>
     public async Task Close(string returnValue)
     {
         if (await DialogIdIsNull()) return;
@@ -47,6 +62,10 @@ public class BlazoredDialog
         await module.InvokeVoidAsync("blazoredDialog.closeWithReturnValue", DialogId, returnValue);
     }
 
+    /// <summary>
+    /// Determines if the dialog is open or not.
+    /// </summary>
+    /// <returns>True if the dialog is open, otherwise False</returns>
     public async Task<bool> IsOpen()
     {
         if (await DialogIdIsNull()) return false;
@@ -54,6 +73,10 @@ public class BlazoredDialog
         return await module.InvokeAsync<bool>("blazoredDialog.isOpen", DialogId);
     }
 
+    /// <summary>
+    /// Sets the return value for the dialog, usually to indicate which button the user pressed to close it.
+    /// </summary>
+    /// <param name="returnValue">Value to be stored in the dialog returnValue.</param>
     public async void SetReturnValue(string returnValue)
     {
         if (await DialogIdIsNull()) return;
@@ -61,6 +84,10 @@ public class BlazoredDialog
         await module.InvokeVoidAsync("blazoredDialog.setReturnValue", DialogId, returnValue);
     }
 
+    /// <summary>
+    /// Gets the return value for the dialog, usually to indicate which button the user pressed to close it.
+    /// </summary>
+    /// <returns>The returnValue associated with the dialog.</returns>
     public async Task<string> GetReturnValue()
     {
         if (await DialogIdIsNull()) return string.Empty;
@@ -68,6 +95,10 @@ public class BlazoredDialog
         return await module.InvokeAsync<string>("blazoredDialog.getReturnValue", DialogId);
     }
 
+    /// <summary>
+    /// Determines if the DialogId is null (has not been set) and logs an error to the web console if not.
+    /// </summary>
+    /// <returns>True if DialogId has been set, otherwise False.</returns>
     private async Task<bool> DialogIdIsNull()
     {
         if (DialogId is null)
